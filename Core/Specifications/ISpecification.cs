1d6dd7af5a213a0d5904b3647c2415d7e8ab6f94
HobotM <mateuszhobot@gmail.com>
2023-09-24 23:59:12 +0100

    Pagination

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -16,6 +16,13 @@ namespace Core.Specifications
 
         Expression<Func<T, object>> OrderByDescending{get;}        
 
+
+        int Take{get;}
+
+        int Skip{get;}
+
+        bool IsPagingEnabled{get;}
+
         
     }
 }
